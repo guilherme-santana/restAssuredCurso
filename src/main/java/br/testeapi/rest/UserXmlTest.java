3ea05ca496ae27ba0a27168bee3d7a6980e35492
guilherme-santana <santana.guilherme93@gmail.com>
2020-05-06 23:54:57 -0300

    devoTrabalharComXml

---
+++ src/main/java/br/testeapi/rest/UserXmlTest.java
@@ -0,0 +1,34 @@
+package br.testeapi.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+public class UserXmlTest {
+	
+	@Test
+	public void devoTrabalharComXml() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/usersXML/3")
+		.then()
+			.statusCode(200)
+			
+			.rootPath("user")
+			.body("name", is("Ana Julia"))
+			.body("@id", is("3"))
+			
+			.rootPath("user.filhos")
+			.body("name.size()", is(2))
+			
+			.detachRootPath("filhos")
+			.body("filhos.name[0]", is("Zezinho"))
+			.body("filhos.name[1]", is("Luizinho"))
+			
+			.appendRootPath("filhos")
+			.body("name", hasItem("Luizinho"))
+			.body("name", hasItems("Luizinho", "Zezinho"))
+		;
+	}
+
+}
