d0e41431cf88e11cb16561cc9bf3b7c604f6e3cc
guilherme-santana <santana.guilherme93@gmail.com>
2020-05-27 22:14:20 -0300

    Buscas em HTML

---
+++ src/main/java/br/testeapi/rest/EnvioDados.java
@@ -0,0 +1,54 @@
+package br.testeapi.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+
+import org.junit.Test;
+
+import io.restassured.http.ContentType;
+
+public class EnvioDados {
+
+	@Test
+	public void deveEnviarValorViaQuery() {
+		given()
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users?format=json")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.JSON)
+		;
+	}
+	
+	@Test
+	public void deveEnviarValorViaQueryViaParam() {
+		given()
+			.log().all()
+			.queryParam("format", "json")
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.JSON)
+			.contentType(containsString("utf-8"))
+		;
+	}
+	
+	@Test
+	public void deveEnviarValorViaQueryViaHeader() {
+		given()
+			.log().all()
+			.accept(ContentType.XML) //Define o tipo do retorno
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.XML)
+		;
+	}
+
+}
