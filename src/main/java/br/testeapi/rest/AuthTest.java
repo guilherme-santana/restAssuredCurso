9acc1000ce4197fa4994dd7fffd16b3110a304f6
guilherme-santana <santana.guilherme93@gmail.com>
2020-07-12 22:42:17 -0300

    Autenticação

--- src/main/java/br/testeapi/rest/AuthTest.java
+++ src/main/java/br/testeapi/rest/AuthTest.java
@@ -1,9 +1,14 @@
 package br.testeapi.rest;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
+
+import io.restassured.http.ContentType;
 
 public class AuthTest {
 	
@@ -21,7 +26,7 @@ public class AuthTest {
 	}
 	
 	@Test
-	public void deveObterClima() {
+	public void deveObterClimaComChave() {
 		given()
 			.log().all()
 			.queryParam("q", "Fortaleza,BR")
@@ -34,6 +39,79 @@ public class AuthTest {
 			.statusCode(200)
 			.body("name", is("Fortaleza"))
 		;
+	}
+	
+	@Test
+	public void naoDeveAcessar() {
+		given()
+			.log().all()
+		.when()
+			.get("http://restapi.wcaquino.me/basicauth")
+		.then()
+			.log().all()
+			.statusCode(401)
+		;
+	}
+	
+	@Test
+	public void deveFazerAutenticacaoBasica() {
+		given()
+			.log().all()
+		.when()
+			.get("http://admin:senha@restapi.wcaquino.me/basicauth")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("status", is("logado"))
+		;
+	}
+	
+	@Test
+	public void deveFazerAutenticacaoBasica2() {
+		given()
+			.log().all()
+			.auth().basic("admin", "senha")
+		.when()
+			.get("http://restapi.wcaquino.me/basicauth")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("status", is("logado"))
+		;
+	}
+	
+	@Test
+	public void deveFazerAutenticacaoComTokenJWT() {
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "santanaguilherme@teste.com");
+		login.put("senha", "123456");
+		
+		//receberToken
+		String token = given()
+			.log().all()
+			.body(login)
+			.contentType(ContentType.JSON)
+		.when()
+			.post("http://barrigarest.wcaquino.me/signin")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.extract().path("token")
+		;
+		
+		//obterContas
+		given()
+			.log().all()
+			.header("Authorization", "JWT "+token)
+		.when()
+			.get("http://barrigarest.wcaquino.me/contas")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("nome", hasItem("Conta de teste"))
+		;
+		
+		
 	}
 
 }
